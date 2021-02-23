477ae1cb8057cf0a04a1627ef7a366173f3b635b
Adrian Duran <duranadria99@gmail.com>
2021-02-24 00:26:03 +0100

    Dado listo2

--- src/js/index.js
+++ src/js/index.js
@@ -26,6 +26,7 @@ const lanzarDado = () =>(Math.floor((Math.random()*6) + 1))
 const printResult = (number,x) => {
     const result = document.getElementById(`number${x.toString()}`);
     result.setAttribute('src',dados[(number-1)])
+    
     result.setAttribute('src','./assets/dado1.png')
         
 }

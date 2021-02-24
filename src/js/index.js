c9216b905682d7004a2fa086a57c6249472bcb57
Adrian Duran <duranadria99@gmail.com>
2021-02-24 18:33:23 +0100

    Dado finalizado

--- src/js/index.js
+++ src/js/index.js
@@ -1,37 +1,35 @@
+import imgDices from "../assets/*.png"
 
-const dado = document.getElementById('lanzar');
-const tablaResult = document.getElementById('results')
-let contador = 1;
+const dice = document.getElementById('lanzar');
+const tableResult = document.getElementById('results')
 
-const dados=["/dado1.78fa86d1.png","/dado2.683e6ac9.png","/dado3.2a8bdebe.png","/dado4.0b4de1d4.png","/dado5.3fb6e328.png","/dado6.a3cab49e.png"]
-
-
-dado.addEventListener('click',()=>{
+//Evento de escucha del clic
+dice.addEventListener('click',()=>{
     const fragment = document.createDocumentFragment();
     const colunm = document.createElement('tr');
     let rows;
-    for(x =1; x<=2;x++){
-        const resultNumber = lanzarDado();
+    for(let x = 1 ; x<=2;x++){
+        const resultNumber = getDice();
         printResult(resultNumber,x);
         rows = printHistory(resultNumber);
         colunm.append(rows);    
     }
 
     fragment.append(colunm);
-    tablaResult.appendChild(fragment);    
+    tableResult.appendChild(fragment);    
 });
 
-const lanzarDado = () =>(Math.floor((Math.random()*6) + 1))
+//Obtener numero random
+const getDice = () =>(Math.floor((Math.random()*6) + 1))
 
+//Colocar imagen del dado
 const printResult = (number,x) => {
     const result = document.getElementById(`number${x.toString()}`);
-    console.log(result.getAttribute("src"))
-    result.setAttribute('src',dados[(number-1)])
-    
-    /* result.setAttribute('src',`./assets/dado${x.toString()}.png`) */
-        
+    result.setAttribute('src',imgDices[`dado${number.toString()}`])
+           
 }
 
+//Imprimir resultado en la tabla
 const printHistory = (content) =>{
     const results = document.createElement('td');
     results.textContent = content
